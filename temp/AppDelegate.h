f1721e361fc47decbf8d5637d39df041ca34e95d
J.K <K_940201@hotmail.com>
2017-08-14 14:09:58 -0700

    Initial Commit

---
+++ temp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  temp
+//
+//  Created by Jaewon Kim on 2017-08-14.
+//  Copyright © 2017 Jaewon Kim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ temp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  temp
+//
+//  Created by Jaewon Kim on 2017-08-14.
+//  Copyright © 2017 Jaewon Kim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
